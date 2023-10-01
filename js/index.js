92aa0e10ab096cd03624059ab5802b3530d6f123
Brianna Renni <briannaxrene@icloud.com>
2023-10-01 04:28:43 -0700

    Rename script, update logic

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const slider = document.querySelector('#slider-bar');
-const viewsElement = document.querySelector('.views');
-const priceElement = document.querySelector('.price');
 const toggle = document.querySelector('#toggle-box');
+const viewsElement = document.querySelector('.views');
+const priceElements = document.querySelectorAll('.price');
 
 const pricingData = [
   { views: '10K', price: 8 },
@@ -12,13 +12,16 @@ const pricingData = [
 ];
 
 viewsElement.textContent = pricingData[2].views;
-priceElement.textContent = `$${pricingData[2].price}.00`;
-console.log(viewsElement.textContent);
+priceElements.forEach((element) => {
+  element.textContent = `$${pricingData[2].price}.00`;
+});
 
 slider.addEventListener('input', function () {
   const index = parseInt(slider.value, 10);
   viewsElement.textContent = pricingData[index].views;
-  priceElement.textContent = `$${pricingData[index].price}.00`;
+  priceElements.forEach((element) => {
+    element.textContent = `$${pricingData[index].price}.00`;
+  });
 });
 
 // If the visitor switches the toggle to yearly billing, a 25% discount should be applied to all prices.
@@ -31,7 +34,9 @@ function updatePrice() {
   }
 
   viewsElement.textContent = pricingData[index].views;
-  priceElement.textContent = `$${price.toFixed(2)}`;
+  priceElements.forEach((element) => {
+    element.textContent = `$${price.toFixed(2)}`;
+  });
 }
 
 toggle.addEventListener('change', updatePrice);
